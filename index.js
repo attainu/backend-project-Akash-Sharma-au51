0d0466bb7ac42e6e974ce8930c6764dd1c990172
https://github.com/attainu/Akash-Sharma-au51 <akashsharmakansaz@gmail.com>
2023-04-08 16:11:02 +0530

    changes made

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(passport.initialize());
 
 app.use(passport.session());
 
-mongoose.connect('mongodb://127.0.0.1:27017/myapp')
+mongoose.connect('mongodb://127.0.0.1:27017/grocery_app')
   .then(() => console.log("database connected"))
   .catch((err) => console.error(err));
 
